8239ecda88f74bbbe6243f663495c5acfcddef00
kmou424 <me@kmou424.moe>
2023-01-16 16:18:43 +0800

    build: covert to library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,15 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("io.ktor.plugin") version "2.2.2"
 }
 
 group = "moe.kmou424"
 version = "0.0.1"
-application {
-    mainClass.set("moe.kmou424.asmrone.api.ApplicationKt")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
 
 repositories {
     mavenCentral()
@@ -30,4 +25,8 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-client-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
