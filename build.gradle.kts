b1e33d7a091b014ad0938145a7d37eacfce5f368
kmou424 <me@kmou424.moe>
2023-01-16 16:19:36 +0800

    chore: drop logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktor_version: String by project
 val kotlin_version: String by project
-val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.0"
@@ -22,7 +21,6 @@ dependencies {
     implementation("io.ktor:ktor-client-logging-jvm:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-jackson-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-client-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
